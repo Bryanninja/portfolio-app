d14ea3f5829f6737c83bb163d21c7d8ec0e093b0
Álex Bryan <bryannascimentopl800@gmail.com>
2026-02-23 14:12:26 -0300

    feat: tailwind colors defined

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,18 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      colors: {
+        portfolio: {
+          white: '#E6F4FD',
+          blueWhite: '#ACDEF2',
+          cyan: '#5BB5D9',
+          lightBlue: '#5496BF',
+          blue: '37648C',
+          darkBlue: '#1D3759',
+          blueText: '#3B465B',
+          darkBlueText: '242B3A',
+        },
+      },
       fontFamily: {
         sans: ['Inter', 'sans-serif'],
       },
